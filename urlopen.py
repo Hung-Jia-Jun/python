ce95d304ebb161b5b94863b7c8bab1dff8ef66fe
JasonHome123 <yf9000555@gmail.com>
2015-12-28 12:48:48 +0800

    Update urlopen.py

--- urlopen.py
+++ urlopen.py
@@ -1,4 +1,10 @@
-import urllib.request
-#更新版後urllib2包在python3內了，所以直接呼叫就可以了
-res=urllib.request.urlopen('http://macloudlab.com/%E8%AA%8D%E8%AD%98facebook-graph-api/')
+import urllib.request#更新版後urllib2包在python3內了，所以直接呼叫就可以了
+value={}
+value['username']='F1021365'
+value['password']='Sai12boat3'
+data =urllib.parse.urlencode(value)
+url = 'http://140.129.253.29/personal/pstudent/login.aspx'
+res=urllib.request.urlopen(url)
+geturl=url+"?"+data
+res=urllib.request.urlopen(geturl)
 res.read()
