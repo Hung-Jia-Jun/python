268acdfa659c26428fadb6d7f774a741badee788
JasonHome123 <yf9000555@gmail.com>
2016-01-19 03:31:20 +0800

    Update 測試請假系統.py
    全部OK完美
    只差例外處理了

--- 測試請假系統.py
+++ 測試請假系統.py
@@ -13,21 +13,28 @@ student_browser=webdriver.Firefox()
 #進入學生資訊網的index
 print u"請輸入登入帳號:"
 account_key = raw_input().decode(sys.stdin.encoding)
-password_key = getpass.getpass().decode(sys.stdin.encoding)
+print u"請輸入學生資訊網密碼:"
+password_key = getpass.getpass("").decode(sys.stdin.encoding)
+
+
 student_browser.get('http://140.129.253.29/Usc/HomePage/flogin.aspx')
 student_browser.find_element_by_name("txtAccount").send_keys(account_key)
 student_browser.find_element_by_name("txtPwd").send_keys(password_key)
 student_browser.find_element_by_css_selector("input[type=\"submit\"]").click()
-##student_browser.find_element_by_link_text(u"案件申請").click()
-student_browser.get('http://140.129.253.29/Usc/Std/Usc_Std_ApplyCase.aspx')
+student_browser.get('http://140.129.253.29/Usc/Std/Usc_Std_ApplyCase.aspx')  #案件申請網頁
 Select(student_browser.find_element_by_name("ApplyUnit")).select_by_visible_text(u"生輔組")
 Select(student_browser.find_element_by_name("ApplyItem")).select_by_visible_text(u"學生請假單")
 student_browser.find_element_by_name("add").click()
-student_browser.find_element_by_link_text(u"聯合服務中心").click()
-Select(student_browser.find_element_by_name("S_Day")).select_by_visible_text("07")#輸入請假開始日
-Select(student_browser.find_element_by_name("E_Day")).select_by_visible_text("07")#輸入請假結束日
-Select(student_browser.find_element_by_name("S_Section")).select_by_visible_text("03")#請假開始節次
-Select(student_browser.find_element_by_name("E_Section")).select_by_visible_text("04")#請假結束節次
+
+
+
+student_browser.switch_to_window(student_browser.window_handles[-1])
+Select(student_browser.find_element_by_name("S_Day")).select_by_visible_text("5")
+Select(student_browser.find_element_by_name("E_Day")).select_by_visible_text("5")
+Select(student_browser.find_element_by_name("S_Section")).select_by_visible_text("07")
+Select(student_browser.find_element_by_name("E_Section")).select_by_visible_text("08")
 Select(student_browser.find_element_by_name("Hcode")).select_by_visible_text(u"病假")
-student_browser.find_element_by_name("Reason").send_keys(u"生病感冒")
+print u"輸入你不小心生病的理由:"
+Reasoninput = raw_input().decode(sys.stdin.encoding)
+student_browser.find_element_by_name("Reason").send_keys(Reasoninput)
 student_browser.find_element_by_css_selector("input[type=\"button\"]").click()
