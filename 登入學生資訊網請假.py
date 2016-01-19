edc1428ad8d2c62e9c18353a3332587bbeab0ce2
JasonHome123 <yf9000555@gmail.com>
2016-01-19 18:11:48 +0800

    Update 登入學生資訊網請假.py
    確定可以執行了

--- 登入學生資訊網請假.py
+++ 登入學生資訊網請假.py
@@ -8,6 +8,7 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re,sys
 from bs4 import BeautifulSoup
 import getpass
+import py2exe
 #指定使用Firefox瀏覽器
 student_browser=webdriver.Firefox()
 def selection_Day_lesson(Daystr,Daystr2,Lessonstr,Lessonstr2):
@@ -22,6 +23,8 @@ def selection_Day_lesson(Daystr,Daystr2,Lessonstr,Lessonstr2):
 	student_browser.find_element_by_name("Reason").send_keys(Reasoninput)
 	student_browser.find_element_by_css_selector("input[type=\"button\"]").click()
 	print u"請假完畢"
+	print u"感謝您的使用,是不是要開啟pixnet說些意見反映給作者我啊??  Y/N"
+	pixnet = raw_input().decode(sys.stdin.encoding)
 	#student_browser.quit()
 	return 0
 
@@ -31,7 +34,8 @@ def selection_Day_lesson(Daystr,Daystr2,Lessonstr,Lessonstr2):
 
 
 print u"--------------------------------------------------------------------------------------"
-print u"歡迎使用台北海洋技術學院自動請假系統，這是由一個每天睡到自然醒的學長寫的"
+print u"歡迎使用台北海洋技術學院自動請假系統"
+print u"這是由一個每天睡到自然醒的學長寫的"
 print u"只要把這個腳本掛到開機啟動資料夾內"
 print u"一起床開電腦後"
 print u"就會幫你找找看哪個老師又記你曠課了，這時此程式就會自動執行"
@@ -62,10 +66,6 @@ except:
 	for i in range(5, 0, -1):
 		student_browser.find_element_by_link_text(u"請假缺曠查詢").click()
 		soup = BeautifulSoup(student_browser.page_source,"html.parser")
-
-
-
-
 print u"查詢完畢"
 
 
@@ -105,24 +105,3 @@ student_browser.find_element_by_name("add").click()
 
 selection_Day_lesson(Daystrip,Daystrip2,Lessonstrstrip,Lessonstrstrip2)
 student_browser.quit()
-
-#這邊是未測試成功的區塊
-"""
-print u"正在查詢請假是否成功......"
-student_browser.get('http://140.129.253.29/Usc/HomePage/flogin.aspx')
-student_browser.find_element_by_name("txtAccount").send_keys(account_key)
-student_browser.find_element_by_name("txtPwd").send_keys(password_key)
-student_browser.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-
-student_browser.get('http://140.129.253.29/Usc/Std/Usc_Std_ApplyCase.aspx')
-print u"進入查詢頁面中......"
-Select(student_browser.find_element_by_name("ApplyUnit")).select_by_visible_text(u"生輔組")
-Select(student_browser.find_element_by_name("ApplyItem")).select_by_visible_text(u"學生請假單")
-student_browser.find_element_by_name("query").click()
-
-
-studentquery=0
-for ele in soup.select('tr'):
-	ele.select('td').text
-"""
