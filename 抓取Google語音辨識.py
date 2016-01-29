4f58739bedc0d7471941fc9549053e6f384ade10
JasonHome123 <yf9000555@gmail.com>
2016-01-29 16:54:05 +0800

    Create 抓取Google語音辨識.py

---
+++ 抓取Google語音辨識.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re,sys
+from bs4 import BeautifulSoup
+import datetime, time
+import io
+cwd="C:\Python27\Scripts\ChromeWebDrive\chromedriver.exe"
+browser=webdriver.Chrome(cwd)
+browser.get('https://www.google.com/intl/en/chrome/demos/speech.html')
+
+
+Select(browser.find_element_by_id("select_language")).select_by_value("36")
+Select(browser.find_element_by_id("select_dialect")).select_by_value("cmn-Hant-TW")
+browser.find_element_by_id("start_button").click()
+time.sleep(10)
+
+browser.find_element_by_id("start_button").click()
+soup = BeautifulSoup(browser.page_source,"html.parser")
+for ele in soup.select('.interim'):
+	print ele.text
+	writeTxtInput=ele.text
+	WriteTxt(writeTxtInput)
+browser.find_element_by_id("start_button").click()
+def WriteTxt(writeText):
+	f = io.open('A.txt', 'w',encoding = 'UTF-8')
+	f.write(writeText)
+	time.sleep(5)
+	f.truncate()
