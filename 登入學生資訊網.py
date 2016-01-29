5a897ab556b3d8dfe83dcf67e8e8cec74d33b898
JasonHome123 <yf9000555@gmail.com>
2016-01-29 17:33:32 +0800

    Delete 登入學生資訊網.py

--- 登入學生資訊網.py
+++
@@ -1,9 +0,0 @@
-import requests
-payload={
-'sID':'XXXXXXXXXX',
-'sPassword':'XXXXXXXXXXXX',
-'btnOk.x':'39',
-'btnOk.y':'6'
-}
-res=requests.post("http://140.129.253.29/personal/pstudent/CheckID.aspx", data=payload)
-res.text
