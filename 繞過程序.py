32e5d1807cad13ffc86a464e3a305b925298f524
JasonHome123 <yf9000555@gmail.com>
2016-03-05 18:26:03 +0800

    Update 繞過程序.py

--- 繞過程序.py
+++ 繞過程序.py
@@ -1,8 +1,11 @@
 import requests
 import urllib2
 from bs4 import BeautifulSoup as bs
-import string,random
-
+import string,random,sys,re
+print u"CSRF token:"
+csrfToken = raw_input().decode(sys.stdin.encoding)
+print u"KKtixToken:"
+kktixToken=raw_input().decode(sys.stdin.encoding)
 def randomword(length):
    return ''.join(random.choice(string.lowercase) for i in range(length))
 
@@ -21,7 +24,7 @@ def  GenPassword(length):
     genPwd =  ''.join([i for  i in  slcChar])
     return  genPwd
 
-my_referer='https://kktix.com/events/scandaltour2016/registrations/new'
+my_referer='https://kktix.com/events/chaiparty-0401/registrations/new'
 header={
 
 "Host":"queue.kktix.com",
@@ -38,30 +41,37 @@ responseChallenge_Total=responseChallenge+responseChallenge2+responseChallenge3
 datas={
     "tickets":
     {
-        "id":48732,
+        "id":48442,
         "quantity":4,
         "invitationCodes":"[]",
         "agreeTerm":"true",
-        "responseField":"185",
-        "responseChallenge":"03AHJ_VuuYvWLlcvdRnVG-qfLlJX0Vr4S4DW2e0f0kv5Y4VqKXA4-c5JG3HCN17So9htpvTMx-gnLKjBW0dhb16hoq6p7A4KmySrzz01Zdei4IP-ObfpPBZrvuxI7V1WX1PE4yGVbVr6hZw24FWA34-1Qa1S2agdys_J7lED91VRfy4v8gj8WuiM-a1HVxtDJTR9LN7dqLGBEiCo9hGHbpwsG5fMjlMK4gcyNwWDGPbXjDx23bJI-OjlYm295LEOlM9CIEqETVjExD"
+        "member_code": "",
+        "use_qualification_id":"null"
+       # "responseField":"185",
+       # "responseChallenge":"03AHJ_VuuYvWLlcvdRnVG-qfLlJX0Vr4S4DW2e0f0kv5Y4VqKXA4-c5JG3HCN17So9htpvTMx-gnLKjBW0dhb16hoq6p7A4KmySrzz01Zdei4IP-ObfpPBZrvuxI7V1WX1PE4yGVbVr6hZw24FWA34-1Qa1S2agdys_J7lED91VRfy4v8gj8WuiM-a1HVxtDJTR9LN7dqLGBEiCo9hGHbpwsG5fMjlMK4gcyNwWDGPbXjDx23bJI-OjlYm295LEOlM9CIEqETVjExD"
     }
 
 }
 #csrfToken = '' .join(randomword(86))
-csrfToken="aD+yE/2yiQ4yT5ylilmk9xfBWtdXiE577Bmt75jAWQRUbdFwkFjASdvu5H7lQh30gOlC090TU/kFjwvtH8O6fw%3D%3D"
 cookie={
-    "Cookie":"__uvt=; uvts=4EmHI70l7SqXHrG8; _ga=GA1.2.1667600123.1456760657; kktix_session_token_v2=e63dbf705590111d8b88733995ea7fc0; __asc=a6d7fad5153390700092508699f; __auc=a8c767e31532db2ff6580c26a55"
+    "Cookie":"__uvt=; uvts=4EmHI70l7SqXHrG8; _ga=GA1.2.1667600123.1456760657; kktix_session_token_v2="+kktixToken+"; __asc=a6d7fad5153390700092508699f; __auc=a8c767e31532db2ff6580c26a55"
+   #"Cookie":"kktix_session_token_v2=005e539745e66292a65b3f120de7d2b8"
+
 }
 s=requests.Session()
 s.headers.update({'Referer':my_referer })
-res=s.post("https://queue.kktix.com/queue/scandaltour2016?authenticity_token=aD%2ByE%2F2yiQ4yT5ylilmk9xfBWtdXiE577Bmt75jAWQRUbdFwkFjASdvu5H7lQh30gOlC090TU%2FkFjwvtH8O6fw%3D%3D",data=datas,headers=header,cookies=cookie)
-#res2=requests.get("https://kktix.com/g/events/2016depapepe/base_info")
-print res.text
-print res.url
+res=s.post("https://queue.kktix.com/queue/scandaltour2016?authenticity_token="+csrfToken,data=datas,headers=header,cookies=cookie)
+ReToText=res.text
+ReToText2=re.sub(r'\u0074\u006f\u006b\u0065\u006e',ReToText)
+print ReToText2
 Random8word=GenPassword(8)
 Random4word_0=GenPassword(4)
 Random4word_1=GenPassword(4)
 Random4word_2=GenPassword(4)
 Random12word=GenPassword(12)
-#res2=s.get("https://queue.kktix.com/queue/token/"+Random8word+'-'+Random4word_0+'-'+Random4word_1+'-'+Random4word_2+'-'+Random12word,headers=header,cookies=cookie)
-#print res2.url
+res2=s.get("https://queue.kktix.com/queue/token/151c9797-72cc-4ba9-a93e-b905f5ab98ac")
+SpiltRes2=res2.text
+#print SpiltRes2.split(':')[1]
+CutSpilt=SpiltRes2.split(':')[1]
+CutSpilt_ok=CutSpilt[1:42]
+print CutSpilt_ok
