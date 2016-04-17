ba3aedc6fe30208a4b6b834911d29cecadd3c963
JasonHome123 <yf9000555@gmail.com>
2016-04-18 02:36:48 +0800

    Update 抓取單一玩家前110紀錄.py
    增加抓取與該玩家關係最接近的10位玩家

--- 抓取單一玩家前110紀錄.py
+++ 抓取單一玩家前110紀錄.py
@@ -1,16 +1,85 @@
 from bs4 import BeautifulSoup
 import requests
-
-res2=requests.get("https://lol.moa.tw/summoner/show/台灣狗狗") #使用者ID網址
-
-soup = BeautifulSoup(res2.text,"html.parser") 
+GameNumCodeStr="https://lol.moa.tw/match/show/"
+def url (ID):
+    ID=str(ID)
+    res2=requests.get("https://lol.moa.tw/summoner/show/"+ID) #使用者ID網址
+    soup = BeautifulSoup(res2.text,"html.parser") 
+    return soup
+def GameAllPlayer (GameNumCode):
+    GameNumCode=str(GameNumCode)
+    res2=requests.get(GameNumCodeStr+GameNumCode) #進入詳細資料網址
+    PlayerNum=0 #定位其餘十位玩家的位置
+    soup = BeautifulSoup(res2.text,"html.parser") 
+    for ele in soup.select('span'):
+        PlayerNum=PlayerNum+1
+        if PlayerNum==22:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==26:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==30:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==34:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==38:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==42:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==46:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==50:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==54:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
+        if PlayerNum==58:
+            PlayerID=str(ele) #剖析使用者ID
+            PlayerID=PlayerID.split(">")[2]
+            PlayerID=PlayerID.split("<")[0]
+            print PlayerID
 def GameTotal(SearchNum,SearchUrl):
     res2=requests.get(SearchUrl)
     soup = BeautifulSoup(res2.text,"html.parser")
     SearchNum=(SearchNum-1)*10
     def GameDetail(GameNum):
         recentKDAnum=0
+        GameNumCode=0 #對戰定位代號
         if GameNum==3 or 4:
+            for ele in soup.select('span'):
+                GameNumCode=GameNumCode+1
+                ele=str(ele)
+                if GameNumCode==((GameNum-1)*10)+3:
+                    ele=str(ele)                    
+                    ele=ele.split('>')[1]
+                    ele=ele.split('<')[0]
+                    print "對戰代號:"+ele
+                    GameAllPlayer(ele)
             for ele in soup.select('tr'):
                 recentKDAnum=recentKDAnum+1
                 if recentKDAnum==1:
@@ -27,6 +96,15 @@ def GameTotal(SearchNum,SearchUrl):
                     GameTime=ele.split(">")[13]
                     print "遊戲長度:"+GameTime.split("<")[0]
         else:
+            
+            for ele in soup.select('span'):
+                GameNumCode=GameNumCode+1
+                if GameNumCode==((GameNum-1)*10)+3:
+                    ele=str(ele)                    
+                    ele=ele.split('>')[1]
+                    ele=ele.split('<')[0]
+                    print "對戰代號:"+ele
+                    GameAllPlayer(ele)
             for ele in soup.select('tr'):
                 recentKDAnum=recentKDAnum+1
                 if recentKDAnum==((GameNum-1)*10)+1:
@@ -42,9 +120,20 @@ def GameTotal(SearchNum,SearchUrl):
 
                     GameTime=ele.split(">")[13]
                     print "遊戲長度:"+GameTime.split("<")[0]
-        recentnum=0
+        recentnum=0 #最近對戰的細節定位代號
+        LegendNum=0 #英雄定位代號
+        for ele in soup.select('td'):
+            if LegendNum==11:
+                ele=str(ele)
+                LegendName=ele.split('''"''')[5]
+                print "使用的英雄:"+LegendName
         for ele in soup.select('td'):
             recentnum=recentnum+1
+            LegendNum=LegendNum+1
+            if LegendNum==((GameNum-1)*10)+1:
+                ele=str(ele)
+                LegendName=ele.split('''"''')[5]
+                print "使用的英雄:"+LegendName
             if recentnum==((GameNum-1)*10)+2:
                 ele=str(ele)
                 print "擊殺:"+ele.split('''"''')[7]
@@ -59,7 +148,7 @@ def GameTotal(SearchNum,SearchUrl):
         print GameNumber+"-------------------------------"
         GameDetail(i)
         print "-------------------------------"
-def SerchUrl():
+def SerchUrl(soup):
     urlStr="https://lol.moa.tw/Ajax/recentgames/"
     urlnum=0 #要剖析的定位行數，利用他到達指定行數後即停止，即可剖析查詢網址
     pagenum=0 #顯示更多對戰的頁碼
@@ -76,5 +165,6 @@ def SerchUrl():
                 SearchUrl=urlStr+"/page:"+pagenum+"/sort:GameMatch.createDate/direction:desc"
                 SearchUrl=str(SearchUrl)
                 GameTotal(SearchNum,SearchUrl)  #SearchNum是搜尋次數，用來方便做出0~100的對戰紀錄，否則只會一直1~10重複10次
-                print SearchUrl
-SerchUrl()
+                #print SearchUrl
+soup=url("小孩子的把戲")
+SerchUrl(soup)
