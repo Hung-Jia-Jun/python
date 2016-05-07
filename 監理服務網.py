0df86d070fb24e9c326b77ba264446821568ec39
JasonHome123 <yf9000555@gmail.com>
2016-05-07 17:27:29 +0800

    Update 監理服務網.py

--- 監理服務網.py
+++ 監理服務網.py
@@ -2,25 +2,43 @@
 from bs4 import BeautifulSoup
 import requests,urllib2
 files={
-"plateNo":"1753-S5",
-"price":"2000",
-"method":"step1",
-"keepQryData":"",
-"CSRFToken":"6089a1ae-3855-4481-9448-8be02d11f1af",
+"memberId":"A126501825",
+"stationCode":"20",
+"plateType":"1",
+"method":"step2",
+"ownerType":"1",
+"processType":"2",
+"hasGetPlate":"N",
+"countyText":"基隆市",
+"area":"仁愛區",
+"zipCode":"200",
+"county":"",
+"location":"",
+"orignPlateNo":"",
+"seriesNoType":"1",
+"carSeriesNo":"WDD2052451F331252",
+"bodyNo":"",
+"engineNo":"",
+"ownerName":"AFAF",
+"ownerUid":"A129162302",
+"CSRFToken":"1148bc5c-231f-4d88-a9b7-7f17ca5e847b",
 } 
 GetCookie={
-    "DWRSESSIONID":"FCqW9jUVXkE$fMklYURPStib5il",
-    "_gat":"1",
-    "JSESSIONID3":"88D261EE1C4274C66C5A5B701B8C8EDA.tsp72",
-    "BSESSIONID1":"B3201A4CC3940A0D6CB9A7F682F72188.tsb12",
-    "MVDAUTH":"TGT-422-chclhmpY2IE0fjmmsdypDzI0oqGxJOdpbdOfKPaIUZZgae96xx-www.mvdis.gov.tw",
+    "DWRSESSIONID":"4yFbuMvFACAPIchVjAXPyUAeail",
+    "MVDAUTH":"TGT-57-fEeMbxtBPLHtfa3zhbRcejLRYXUPjhqIeXs02fIa5xQDvACEH4-www.mvdis.gov.tw",
     "M3UID":"A126501825",
-    "JSESSIONID1":"C526E7140D02B4E812B70B85341EB87D.tsp12",
-    "_ga":"GA1.3.857149626.1462496845",
+    "BSESSIONID1":"A7FD509506333A6CB341416AC96CD573.tsb11",
+    "_gat":"1",
+    "JSESSIONID3":"E221F4B060D677896D7F2524A32384A8.tsp72",
+    "JSESSIONID1":"004BF1A6BA34D15EBF7A6E7DE1B74B41.tsp12",
+    "_ga":"GA1.3.224652041.1462607619",
     "M3UID":"A126501825"
 }
+header={
+"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"
+}
 s=requests.Session()
-
-res=s.post("https://www.mvdis.gov.tw/m3-emv-plate/webpickno/member/operatePickNo#anchor",files=files,cookies=GetCookie,verify=False)
+s.headers.update({'Referer':"https://www.mvdis.gov.tw/m3-emv-plate/webpickno/queryPickNo" })
+res=s.post("https://www.mvdis.gov.tw/m3-emv-plate/webpickno/member/operatePickNo#anchor",headers=header,data=files,cookies=GetCookie,verify=False)
 soup = BeautifulSoup(res.text,"html.parser")
 print soup.text
