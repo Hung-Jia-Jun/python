175497f97b2f2e2ef98e3829a1416ee5137f9714
yf9000555 <yf9000555@gmail.com>
2016-05-20 22:12:00 +0800

    拿掉發音的模組 稍後補上

--- 抓取Google語音辨識.py
+++ 抓取Google語音辨識.py
@@ -9,16 +9,18 @@ import unittest, time, re,sys
 from bs4 import BeautifulSoup
 import datetime, time
 import io
+import os
+
+
 def WriteTxt(writeTxtInput):
-	f = io.open('D:\Speech recognition5.3\Assets\A.txt', 'w',encoding = 'UTF-8')
+	f = io.open('C:\A.txt', 'w',encoding = 'UTF-8')
 	f.write(writeTxtInput)
 	time.sleep(5)
 	f.truncate()
 cwd="C:\Python27\Scripts\WebDrive\chromedriver.exe"
+
 browser=webdriver.Chrome(cwd)
 browser.get('https://www.google.com/intl/en/chrome/demos/speech.html')
-
-
 Select(browser.find_element_by_id("select_language")).select_by_value("36")
 Select(browser.find_element_by_id("select_dialect")).select_by_value("cmn-Hant-TW")
 browser.find_element_by_id("start_button").click()
@@ -26,9 +28,20 @@ time.sleep(10)
 
 browser.find_element_by_id("start_button").click()
 soup = BeautifulSoup(browser.page_source,"html.parser")
-for ele in soup.select('.interim'):
+for ele in soup.select('.final'):
 	print ele.text
 	writeTxtInput=ele.text
-WriteTxt(writeTxtInput)
-for i in io.open('D:\Speech recognition5.3\Assets\A.txt', 'r',encoding = 'UTF-8'):
-  print i
+	WriteTxt(writeTxtInputcut)
+
+
+for loop in range(10, 0, -1):
+	os.system("pause")
+	browser.find_element_by_id("start_button").click()
+	time.sleep(5)
+	browser.find_element_by_id("start_button").click()
+	soup = BeautifulSoup(browser.page_source,"html.parser")
+	for ele in soup.select('.final'):
+		print ele.text
+		writeTxtInput=ele.text
+		WriteTxt(writeTxtInputcut)
+browser.close()
