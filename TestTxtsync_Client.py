4e6baced0540ee84c72527c7b65fb86f771b0f4b
JasonHome123 <yf9000555@gmail.com>
2016-07-19 17:02:07 +0800

    Add files via upload

--- TestTxtsync_Client.py
+++ TestTxtsync_Client.py
@@ -1,35 +1,77 @@
 # -*- coding: utf-8 -*-
 from socket import *
-HOST = 'localhost' #IP位置看  Cmd 輸入指令ipconfig 查看IPv4位置貼上來
-PORT = 21567
-BUFSIZ = 1024
-ADDR = (HOST,PORT)
-
-tcpCliSock = socket(AF_INET,SOCK_STREAM)
-tcpCliSock.connect(ADDR)
-def WriteSoundTxt(writeTxtInput):
-	f = open('C:\\PlaySound_Sync.txt', 'w') #要同步的playsound.txt
-	f.write(writeTxtInput)
-	f.truncate()
+import multiprocessing,pdb
 User_Sound=""
-def ReadSoundTxt():
-	global User_Sound
-	for i in open('C:\\PlaySound_Sync.txt','r'):
-		User_Sound=i
-	if User_Sound=="":
-		pass
-	return User_Sound
-Tempdata=""
-while True:
-	data=ReadSoundTxt()
-	#message = tcpCliSock.recv(BUFSIZ)
-	if data=="":
-		pass
-	else:
-		if (data!=Tempdata): #資料較新
-			Tempdata=data #將現在的資料存進Tempdata暫存器
-			tcpCliSock.send(data)
-		#elif (data!=message): #如果client端的資料=data 跟伺服器發送過來的message不一樣的話，就代表來自Server的message資料比較新
-		#	WriteSoundTxt(message) #寫入來自server的新資料
-		#	print "[Get From Server]:",message
-tcpCliSock.close()
+BUFSIZ = 1024
+
+def WriteSoundTxt(FileLocation,writeTxtInput):
+        f = open(FileLocation, 'w') #要同步的playsound.txt
+        f.write(writeTxtInput)
+        f.close() #關閉文件流
+def ReadSoundTxt(FileLocation):
+    global User_Sound
+    for i in open(FileLocation,'r'):
+        User_Sound=i
+    if User_Sound=="":
+        pass
+    return User_Sound
+
+def RunThread(SocketTo):
+	PlaySound_console=WriteSoundTxt('C:\\A_Client.txt',"") #伺服器端的A.txt辨識指令檔
+	Server_Sound_Data=WriteSoundTxt('C:\\PlaySound_Client.txt',"") #伺服器端的Play_Sound.txt
+	WriteSoundTxt('C:\\RecogContant_Client.txt',"")#寫入新資料
+
+
+
+	PORT = 47990
+	ADDR = (SocketTo,PORT) #IP位置看  Cmd 輸入指令ipconfig 查看IPv4位置貼上來
+	tcpCliSock = socket(AF_INET,SOCK_STREAM)
+	tcpCliSock.connect(ADDR)
+	Tempdata=""
+	Console_Tempdata=""
+	Temp_RecogContant=""
+	while True:
+		#pdb.set_trace()
+
+		Recv_Server_Message=tcpCliSock.recv(BUFSIZ) #接收server指令
+		tcpCliSock.send(" ")#發送空值不然server會一直等待
+
+		if Recv_Server_Message[0:10]=="Sound_Data":
+			message=Recv_Server_Message.split("Sound_Data")[1]
+			if (Tempdata!=message): #如果server給的資料與現在的暫存器內容不一樣
+				print u"[Server PlaySound ]: "+message
+				Tempdata=message #將現在的資料存進Tempdata暫存器
+				WriteSoundTxt('C:\\PlaySound_Client.txt',message)#寫入新資料
+
+		elif Recv_Server_Message[0:17]=="Playsound_console":
+			Console_Message=Recv_Server_Message.split("Playsound_console")[1]
+			if (Console_Tempdata!=Console_Message): #如果server給的資料與現在的暫存器內容不一樣
+				print u"[Server python Console]: "+Console_Message
+				Console_Tempdata=Console_Message #將現在的資料存進Tempdata暫存器
+				WriteSoundTxt('C:\\A_Client.txt',Console_Message)#寫入新資料
+
+
+
+
+		RecogContant=ReadSoundTxt('C:\\RecogContant_Client.txt') #client端的辨識結果要回傳給server 所以一直去讀取辨識結果內容
+		if RecogContant=="":
+		    pass
+		else:
+		    if (RecogContant!=Temp_RecogContant): #如果發送前的資料 與上次發送的內容部不一樣
+		        Temp_RecogContant=RecogContant;#把現在這個較新的資料存入暫存器
+		        print "RecogContant Send..."
+		        try:
+		            tcpCliSock.send("RecogContant"+RecogContant) #發送更新的伺服器發聲命令
+		        except :
+		            pass
+
+
+
+
+	tcpCliSock.close()
+if __name__ == '__main__':
+	print u"[遠端連線的主機] : "
+	#SocketTo="localhost"
+	#SocketTo=raw_input()
+	SocketTo="163.18.62.27"
+	RunThread(SocketTo)
--- TestTxtsync_Server.py
+++ TestTxtsync_Server.py
@@ -1,47 +1,107 @@
 # -*- coding: utf-8 -*-
 from socket import *
-HOST = ''
-PORT = 21567
-BUFSIZ = 1024
-ADDR = (HOST,PORT)
-
-tcpSerSock = socket(AF_INET, SOCK_STREAM)
-tcpSerSock.bind(ADDR)
-tcpSerSock.listen(5)
-def WriteSoundTxt(writeTxtInput):
-    f = open('C:\\PlaySound.txt', 'w')
-    f.write(writeTxtInput)
-    f.truncate()
+import multiprocessing,os,sys,pdb
+BUFSIZ=1024
 User_Sound=""
-def ReadSoundTxt():
+
+
+
+
+def WriteSoundTxt(FileLocation,writeTxtInput):
+        f = open(FileLocation, 'w') #要同步的playsound.txt
+        f.write(writeTxtInput)
+        f.close() #關閉文件流
+def ReadSoundTxt(FileLocation):
     global User_Sound
-    for i in open('C:\\PlaySound.txt','r'):
+    for i in open(FileLocation,'r'):
         User_Sound=i
     if User_Sound=="":
         pass
     return User_Sound
-Temp_Server_Data=""
-while True:
-    print 'waiting for connection...'
-    tcpCliSock,addr = tcpSerSock.accept()
-    print '...connected from: ',addr
 
+def RunThread():
+    PlaySound_console=WriteSoundTxt('C:\\A.txt',"") #伺服器端的A.txt辨識指令檔
+    Server_Sound_Data=WriteSoundTxt('C:\\PlaySound.txt',"") #伺服器端的Play_Sound.txt
+    WriteSoundTxt('C:\\RecogContant.txt',"")#寫入新資料
+
+    HOST = ''
+    PORT = 47990
+
+    ADDR = (HOST,PORT)
+    tcpSerSock = socket(AF_INET, SOCK_STREAM)
+    tcpSerSock.bind(ADDR)
+    tcpSerSock.listen(5)
+    User_Sound=""
+    Temp_Server_Data=""
+    Temp_Sound_Console=""
+    Temp_RecogContant=""
+    Recv_Client_Message=""
+    Tempdata=""
     while True:
-        #message = raw_input("Service>")
-        #tcpCliSock.send(message)
-        data = tcpCliSock.recv(BUFSIZ)
-        print "[From Client]:",data
+        #pdb.set_trace()
+        print 'waiting for Client connection...'
+        tcpCliSock,addr = tcpSerSock.accept()
+        print 'connected Client from: ',addr,".................."
+        while True:
+            #pdb.set_trace()
+
+            try:
+                tcpCliSock.send(" ")#測試connet狀態
+                Recv_Client_Message=tcpCliSock.recv(BUFSIZ) #接收Client python指令
+                if Recv_Client_Message=="Clinet Connet":
+                    Recv_Client_Message=""
+            except:
+                break #進入等待client的迴圈等待連線
+
+            PlaySound_console=ReadSoundTxt('C:\\A.txt') #伺服器端的A.txt辨識指令檔
+            Server_Sound_Data=ReadSoundTxt('C:\\PlaySound.txt') #伺服器端的Play_Sound.txt
+            if Server_Sound_Data=="":
+                pass
+            else:
+                if (Server_Sound_Data!=Temp_Server_Data): #如果發送前的資料 與上次發送的內容部不一樣
+                    Temp_Server_Data=Server_Sound_Data;#把現在這個較新的資料存入暫存器
+                    #Server_Sound_Data="Sound_Data"+Server_Sound_Data
+                    print "Sound_Data Send..."
+                    try:
+                        tcpCliSock.send("Sound_Data"+Server_Sound_Data) #發送更新的伺服器發聲資料
+                    except :
+                        pass
 
 
-        if (data!=Server_Sound_Data): #如果進來的資料=data  跟伺服器Server_Sound_Data不一樣
-            Temp_Server_Data=data;#存入暫存器
-            WriteSoundTxt(data) #寫入新資料
+            if PlaySound_console=="":
+                pass
+            else:
+                if (PlaySound_console!=Temp_Sound_Console): #如果發送前的資料 與上次發送的內容部不一樣
+                    Temp_Sound_Console=PlaySound_console;#把現在這個較新的資料存入暫存器
+                    print "Playsound_console Send..."
+                    try:
+                        tcpCliSock.send("Playsound_console"+PlaySound_console) #發送更新的伺服器發聲命令
+                    except :
+                        pass
 
-        Server_Sound_Data=ReadSoundTxt() #伺服器端的Play_Sound.txt
 
 
-        if (Server_Sound_Data!=Temp_Server_Data)
-        #if (Server_Sound_Data!=Temp_Server_Data): #如果今天伺服器這邊的資料比暫存器的資料還新的話
-        #    tcpCliSock.send(Server_Sound_Data) #發送較新的數據給Client端
-        #    print Server_Sound_Data
-tcpSerSock.close()
+            if Recv_Client_Message[0:12]=="RecogContant":
+                message=Recv_Client_Message.split("RecogContant")[1]
+                if (Tempdata!=message): #如果server給的資料與現在的暫存器內容不一樣
+                    print u"Get RecogContant~ "
+                    Tempdata=message #將現在的資料存進Tempdata暫存器
+                    WriteSoundTxt('C:\\RecogContant.txt',message)#寫入新資料
+
+
+
+
+
+
+
+
+
+
+    tcpSerSock.close()
+
+
+
+
+
+RunThread()
+
