a343b01b6871401efbaade1b2a1fc425120aa0ee
JasonHome123 <yf9000555@gmail.com>
2016-07-31 10:09:02 +0800

    Update 學生資訊網.py

--- 學生資訊網.py
+++ 學生資訊網.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 s=requests.Session()
 data={
-    "sID":"F1021365",
-    "sPassword":"Sai12boat3",
+    "sID":"XXXXXX",
+    "sPassword":"XXXXXX",
 }
 res=s.post("http://140.129.253.29/personal/pstudent/CheckID.aspx?urF1021352",data=data)
 print res.text
