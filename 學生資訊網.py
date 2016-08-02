9e7faa0abfc6a10ea11fa25147e2f7b79720dd4a
JasonHome123 <yf9000555@gmail.com>
2016-08-02 19:44:13 +0800

    Delete 學生資訊網.py

--- 學生資訊網.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-s=requests.Session()
-data={
-    "sID":"XXXXXX",
-    "sPassword":"XXXXXX",
-}
-res=s.post("http://140.129.253.29/personal/pstudent/CheckID.aspx?urF1021352",data=data)
-print res.text
