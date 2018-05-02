178b4dd6fd7e01105c64b1cd57b58decab238bc4
洪嘉駿 <yf9000555@gmail.com>
2018-05-02 20:01:34 +0800

    Update LineBOTServer.py

--- LineBOTServer.py
+++ LineBOTServer.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import os,json
 from linebot import (
 	LineBotApi, WebhookHandler
 )
@@ -17,6 +18,7 @@ from linebot.models import (
 	PostbackTemplateAction,
 	MessageTemplateAction,
 	URITemplateAction,
+	ConfirmTemplate,
 	ImageCarouselTemplate,
 	ImageCarouselColumn,
 	CarouselColumn,
@@ -24,9 +26,18 @@ from linebot.models import (
 import pdb
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('Pk6Wrq32ctiw/gk/7/fPT3/VHXoINfdCk1mLf5OkKygSREGK7M9xuj1AoJImmAye1u42ad+UMUlZOBfoTkvxLmddKFaOf3mrpE6fyp75Wx8d2Q7a18kkWU9oW42sJsDg7lM/MuzX5ic4dztlhtQEjgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('f6cc92ff12e1b2bc625b8a2604918117')
-
+"""
+ACCESS_TOKEN="Pk6Wrq32ctiw/gk/7/fPT3/VHXoINfdCk1mLf5OkKygSREGK7M9xuj1AoJImmAye1u42ad+UMUlZOBfoTkvxLmddKFaOf3mrpE6fyp75Wx8d2Q7a18kkWU9oW42sJsDg7lM/MuzX5ic4dztlhtQEjgdB04t89/1O/w1cDnyilFU="
+SECRET="f6cc92ff12e1b2bc625b8a2604918117"
+PORT=5000
+HOST='127.0.0.1'
+"""
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+SECRET = os.environ.get('SECRET')
+PORT=int(os.environ.get('PORT'))
+HOST=os.environ.get('HOST')
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(SECRET)
 
 @app.route("/", methods=['POST'])
 def index():
@@ -45,10 +56,10 @@ def index():
 
 	return 'OK'
 
-@app.route('/PushMsg/<username>')
-def PushMsg(username):
-	line_bot_api.multicast(['Ue2a91beba135696d6b7cafad52e51587'], 
-	TextSendMessage(text=username))
+@app.route('/PushMsg/')
+def PushMsg():
+	#line_bot_api.multicast(['Ue2a91beba135696d6b7cafad52e51587'], 
+	#TextSendMessage(text=username))
 	return "OK"
 
 def CarouseObj_MessageTemplate(item): #輪播圖子物件  全文字回傳
@@ -104,6 +115,7 @@ def handle_follow(event):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+	carousel_template_message=""
 	if event.message.text=="班級查詢/報名":
 		carousel_template_message = CreateCarouselTemplate([
 				["https://www.taiwan.net.tw/resources/images/Attractions/0001095.jpg",
@@ -190,6 +202,49 @@ def handle_message(event):
 					]
 				],
 			])
+	if event.message.text in ["台北市中正區","台北市大同區","台北市中山區","台北市松山區","台北市大安區","台北市萬華區","台北市信義區","台北市士林區","台北市北投區","台北市內湖區","台北市南港區","台北市文山區"]:
+		carousel_template_message = CreateCarouselTemplate([
+				["https://www.taiwan.net.tw/resources/images/Attractions/0001095.jpg",
+					"法會/活動",
+					"確定選擇區域",#description1
+					[
+						["地點確定，我要報名","地點確定，我要報名"],
+						["主題選單","主題選單"],
+						[" "," "],
+					]
+				],
+			])
+		
+	if event.message.text=="地點確定，我要報名":
+		carousel_template_message = CreateCarouselTemplate([
+				["https://www.taiwan.net.tw/resources/images/Attractions/0001095.jpg",
+					"法會/活動",
+					"個人資料填寫",#description1
+					[
+						["我有報名過活動","我有報名過活動"],
+						["我沒有報名過活動","我沒有報名過活動"],
+						["主題選單","主題選單"],
+					]
+				],
+			])
+		
+	if event.message.text=="我沒有報名過活動":
+		carousel_template_message = TextSendMessage(text="請輸入您的姓名?\n並以\n報名人是王小明\n的格式回傳")
+		
+	if event.message.text=="重新報名":
+		carousel_template_message = TextSendMessage(text="請輸入您的姓名?\n並以\nn報名人是王小明\n的格式回傳")
+			
+		
+		
+	if "報名人是" in event.message.text:
+		CheckName=event.message.text.split("報名人是")[1]
+		carousel_template_message=TextSendMessage(text=CheckName+"\n您好 請選擇要上的時間\n並以\n我要在年/月/日/時/分的時候上課\n的格式回傳")
+	
+	if "的時候上課" in event.message.text:
+		carousel_template_message=TextSendMessage(text="報名成功!!!")
+		
+
+
 	if event.message.text=="主題選單":
 		carousel_template_message = CreateCarouselTemplate(
 			[
@@ -244,11 +299,14 @@ def handle_message(event):
 				],
 			]
 		)
-	line_bot_api.reply_message(event.reply_token,carousel_template_message)#,TextSendMessage(text=event.message.text))
-	profile = line_bot_api.get_profile(str(event.source).split("userId")[1].split('"')[2])
-	print(profile.display_name)
-	print(profile.user_id)
+	if carousel_template_message=="":
+		line_bot_api.reply_message(event.reply_token,TextSendMessage(text="你說什麼呢? 我聽不懂"))
+	else:
+		line_bot_api.reply_message(event.reply_token,carousel_template_message)#,TextSendMessage(text=event.message.text))
+		profile = line_bot_api.get_profile(str(event.source).split("userId")[1].split('"')[2])
+		print(profile.display_name)
+		print(profile.user_id)
 
 if __name__ == "__main__":
 	app.debug = True
-	app.run(host='127.0.0.1', port= 5000)
+	app.run(host=HOST,port=PORT)
