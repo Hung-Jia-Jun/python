28eaf5d4d09473f6146fb5d3a7e862eeca88a8fe
洪嘉駿 <yf9000555@gmail.com>
2019-01-19 17:22:47 +0800

    開啟一個攝像機
    測試基礎相機的自動對焦功能
    就直接開啟一個視窗出來顯示
    
    應用於 Ubuntu系統 Work OK!!!

---
+++ OpencvDetect/CaptureWebCameraStream.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+import cv2
+
+# 選擇第二隻攝影機
+cap = cv2.VideoCapture(0)
+
+while(True):
+  # 從攝影機擷取一張影像
+  ret, frame = cap.read()
+
+  # 顯示圖片
+  cv2.imshow('frame', frame)
+
+  # 若按下 q 鍵則離開迴圈
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+# 釋放攝影機
+cap.release()
+
+# 關閉所有 OpenCV 視窗
+cv2.destroyAllWindows()
