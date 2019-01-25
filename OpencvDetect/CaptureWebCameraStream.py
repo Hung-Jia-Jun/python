2e7bc2c7f0dbeb7feccc9ba225d39d7e52a2afb0
洪嘉駿 <yf9000555@gmail.com>
2019-01-25 14:32:52 +0800

    增加FTP上傳功能
    現在能自動變焦，而且按下鍵盤 英文字母c
    可以拍照後上傳FTP
    FTP位置可以自由指定，內外網都行

--- OpencvDetect/CaptureWebCameraStream.py
+++ OpencvDetect/CaptureWebCameraStream.py
@@ -1,5 +1,25 @@
 # -*- coding: UTF-8 -*-
 import cv2
+import ftplib
+import os
+import multiprocessing
+import time
+Username = " "
+Password = " "
+def upload(file):
+	ftp = ftplib.FTP("files.000webhost.com")
+	ftp.login(Username,Password)
+	ext = os.path.splitext(file)[1]
+	if ext in (".txt", ".htm", ".html"):
+		ftp.storlines("STOR " + file, open(file))
+	else:
+		ftp.storbinary("STOR " + file, open(file, "rb"), 1024)
+	os.remove(FileName)
+	print (FileName+" uploaded")
+
+
+
+
 
 # 選擇第二隻攝影機
 cap = cv2.VideoCapture(0)
@@ -11,9 +31,11 @@ while(True):
   # 顯示圖片
   cv2.imshow('frame', frame)
 
-  # 若按下 q 鍵則離開迴圈
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+  if cv2.waitKey(1) & 0xFF == ord('c'):
+	FileName = time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(time.time()))+'.png'
+	cv2.imwrite(FileName,frame)
+	multiprocessing.Process(target=upload,args=(FileName,) ).start()
+	
 
 # 釋放攝影機
 cap.release()
