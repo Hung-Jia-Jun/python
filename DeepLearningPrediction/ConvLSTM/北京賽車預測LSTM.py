26d954fd8bda5230aaedc5dc0dbcf8c7d2b4f760
洪嘉駿 <yf9000555@gmail.com>
2019-06-16 19:55:13 +0800

    Update 北京賽車預測LSTM.py

--- DeepLearningPrediction/ConvLSTM/北京賽車預測LSTM.py
+++ DeepLearningPrediction/ConvLSTM/北京賽車預測LSTM.py
@@ -1,24 +1,3 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
-from sklearn.preprocessing import StandardScaler  
-a = np.array([1,2,3,4,5,6,7,8,9,10])
-scaler = MinMaxScaler(feature_range=(0, 1))
-
-a = numpy.reshape (a,(-1,1))
-a = scaler.fit_transform(a)
-print (a)
-
-a = scaler.inverse_transform(a)
-print (a)
-
-
-# In[19]:
-
-
 # LSTM for international airline passengers problem with regression framing
 import numpy
 import matplotlib.pyplot as plt
@@ -29,19 +8,10 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-import pdb
+from keras.layers.convolutional_recurrent import ConvLSTM2D
+from keras.layers.core import Flatten
 
-import numpy as np
-
-import keras.backend.tensorflow_backend as KTF
-import tensorflow as tf
-config = tf.ConfigProto()  
-config.gpu_options.allow_growth=True   
-session = tf.Session(config=config)
-KTF.set_session(session)
-
-
-# 產生 (X, Y) 資料集, Y 是下一期的乘客數
+#產生X學習集 與Y特徵集
 def create_dataset(dataset, look_back=10):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
@@ -51,25 +21,15 @@ def create_dataset(dataset, look_back=10):
 	return numpy.array(dataX), numpy.array(dataY)
 
 # 載入訓練資料
-dataframe = read_csv('C:\\Users\\Jason\\Anaconda3\\Scripts\\Bejin_Car_LSTM\\ConvLSTM\\pk10.csv', usecols=[2,3,4,5,6,7,8,9,10,11], engine='python', skipfooter=0)
-#dataframe = read_csv('/Users/Jason/Scripts/ConvLSTM/Airplan.csv', usecols=[1], engine='python', skipfooter=3)
+dataframe = read_csv('pk10.csv', engine='python')
 
 dataset = dataframe.values
+dataset = dataset.flatten().reshape(-1, 1)
 dataset = dataset.astype('float32')
-
-#使用前1000個資料，因為運算量巨大，會算太久
 dataset = dataset[:100]
-
-#扁平化所有資料，這樣資料就能以一維參數做預測學習他的排列組合模式
-dataset = dataset.flatten()
-
-#轉換Array維度，讓fit_transform可以運作
-dataset = numpy.reshape (dataset,(-1,1))
-
 
 # 正規化(normalize) 資料，使資料值介於[0, 1]
 scaler = MinMaxScaler(feature_range=(0, 1))
-
 dataset = scaler.fit_transform(dataset)
 
 # 2/3 資料為訓練資料， 1/3 資料為測試資料
@@ -77,21 +37,17 @@ train_size = int(len(dataset) * 0.67)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 
-# 產生 (X, Y) 資料集, Y 是下一個數字(reshape into X=t and Y=t+1)
+
+# 產生 (X, Y) 資料集
+#X值是前面10筆資料  Y值是希望 LSTM去預測出的結果
 look_back = 1
-
-
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
 # reshape input to be [samples, time steps, features]
+
+
 trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-import pdb
-pdb.set_trace()
-print ("OK")
-
-
-# In[20]:
 
 
 
@@ -100,61 +56,12 @@ model = Sequential()
 model.add(LSTM(4, input_shape=(1, look_back)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+model.fit(trainX, trainY, epochs=200, batch_size=1, verbose=2)
 
 
-# In[23]:
-
-
-import pdb
-import numpy as np
 # 預測
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
-
-
-
-#trainPredict = scaler.inverse_transform(trainPredict)
-#testPredict = scaler.inverse_transform(testPredict)
-
-
-# invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-testPredict = scaler.inverse_transform(testPredict)
-
-
-pdb.set_trace()
-
-
-trainY = scaler.inverse_transform([trainY])
-testY = scaler.inverse_transform([testY])
-
-
-trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-
-
-
-print (trainPredict)
-
-"""
-scaler.inverse_transform(numpy.reshape (trainX.flatten(),(-1,1)))
-
-scaler.inverse_transform(numpy.reshape (trainPredict.flatten(),(-1,1)))
-"""
-
-"""
-# invert predictions
-trainPredict = scaler.inverse_transform(trainPredict)
-trainY = scaler.inverse_transform([trainY])
-testPredict = scaler.inverse_transform(testPredict)
-testY = scaler.inverse_transform([testY])
-"""
-
-
-# In[ ]:
-
-
 
 # 回復預測資料值為原始數據的規模
 trainPredict = scaler.inverse_transform(trainPredict)
@@ -186,4 +93,3 @@ plt.plot(scaler.inverse_transform(dataset))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
 plt.show()
-
