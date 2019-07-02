41fd5b93ad908a31ec9e86b8fb727ac1dfe3609b
洪嘉駿 <yf9000555@gmail.com>
2019-07-02 18:13:41 +0800

    Update fetchWebServiceData.py

--- LinePayDjangoServer/LinepayAPP/fetchWebServiceData.py
+++ LinePayDjangoServer/LinepayAPP/fetchWebServiceData.py
@@ -1,6 +1,6 @@
 import zeep
 import json
-wsdl = 'http://60.248.91.143/services/nahooWebservice?WSDL'
+wsdl = 'http://XXX.XXX.XXX.X/services/nahooWebservice?WSDL'
 client = zeep.Client(wsdl=wsdl)
 shopInfo = client.service.shopInfo('10')
 shopInfo = json.loads(shopInfo)
